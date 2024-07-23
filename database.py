710d65536ec7cca0e683862acb3897742cf7acb9
[Pedro Figueredo] <ph71542@gmail.com>
2024-07-23 20:04:51 -0300

    Adicionando as dependencias da cors no sistema

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 #initialise flask api
 app = Flask(__name__)
+CORS(app, origins='http://localhost:5173/')
+
 
 #configure database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://your_username:your_password@localhost/sis_social'
