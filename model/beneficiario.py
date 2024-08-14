fca65f41abbedea40edd3a3e4db53ee896e4dcad
[Pedro Figueredo] <ph71542@gmail.com>
2024-08-14 14:57:13 -0300

    Alterando beneficiário e serviços

--- model/beneficiario.py
+++ model/beneficiario.py
@@ -1,3 +1,4 @@
+from sqlalchemy import DateTime
 from database import db
 
 class Beneficiario(db.Model):
@@ -5,14 +6,20 @@ class Beneficiario(db.Model):
     
     name_ben = db.Column(db.String(50), nullable=False)
     cpf = db.Column(db.String(11), nullable=False, primary_key=True)
-    services = db.Column(db.String(50), nullable=False)
+    nascimento = db.Column(DateTime, nullable=False)
+    telefone = db.Column(db.String(16))
+    endereco = db.Column(db.String(50))
+    servicos = db.Column(db.String(50), nullable=False)
     user_ben = db.Column(db.String(50), nullable=False)
     password_ben = db.Column(db.String(50), nullable=False)
     
-    def __init__(self, name_ben, cpf, services, user_ben, password_ben):
+    def __init__(self, name_ben, cpf, nascimento, telefone, endereco, servicos, user_ben, password_ben):
         self.name_ben = name_ben
         self.cpf = cpf
-        self.services = services
+        self.nascimento = nascimento
+        self.telefone = telefone
+        self.endereco = endereco
+        self.servicos = servicos
         self.user_ben = user_ben
         self.password_ben = password_ben
         
@@ -20,38 +27,10 @@ class Beneficiario(db.Model):
         return {
             'name_ben': self.name_ben,
             'cpf': self.cpf,
-            'services': self.services,
+            'nascimento': self.nascimento,
+            'telefone': self.telefone,
+            'endereco': self.endereco,
+            'servicos': self.servicos,
             'user_ben': self.user_ben,
             'password_ben': self.password_ben
         }
-
-
-
-
-# CODIFO FUNCIONANDO
-# from database import db
-
-# class Beneficiario(db.Model):
-#     __tablename__ = 'beneficiario'
-    
-#     name_ben = db.Column(db.String(50), nullable=False)
-#     cpf = db.Column(db.String(11), nullable=False, primary_key=True)
-#     services = db.Column(db.String(50), nullable=False)
-#     user_ben = db.Column(db.String(50), nullable=False)
-#     password_ben = db.Column(db.String(50), nullable=False)
-    
-#     def __init__(self, name_ben, cpf, services, user_ben, password_ben):
-#         self.name_ben = name_ben
-#         self.cpf = cpf
-#         self.services = services
-#         self.user_ben = user_ben
-#         self.password_ben = password_ben
-        
-#     def to_dictionary(self):
-#         return {
-#             'name_ben': self.name_ben,
-#             'cpf': self.cpf,
-#             'services': self.services,
-#             'user_ben': self.user_ben,
-#             'password_ben': self.password_ben
-#         }
